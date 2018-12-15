b7501da6a0532a407f7a9c79d5d421f246a7e664
Mark <mark.faraj86@gmail.com>
2018-12-15 16:55:13 -0600

    finalizing the program

--- liri.js
+++ liri.js
@@ -2,32 +2,34 @@
 require("dotenv").config();
 
 //Require data from moment npm package
-var moment = require('moment');
+let moment = require('moment');
 //Require data from File System npm package
-var fs = require("fs");
+let fs = require("fs");
 //Require data from Axios npm package
-var axios = require("axios");
+let axios = require("axios");
 //Grab data key from keys.js file
-var keys = require("./keys");
+let keys = require("./keys");
 //Require data from node-API npm package
-var Spotify = require('node-spotify-api');
+let Spotify = require('node-spotify-api');
 //Store spotify API key 
-var spotify = new Spotify(keys.spotify);
+let spotify = new Spotify(keys.spotify);
 //Store the second argument from the command line
-var nodeArgs = process.argv[3];
+let nodeArgs = process.argv.slice(3).join("+")//process.argv[3];
+//store the 1st argument to the end from the comman line
+let command = process.argv.slice(2).join(" ");
 //Store the first argument from the command line
-var program = process.argv[2];
-
+let program = process.argv[2];
+//adding this message
 
 //Program conditions 
 switch (program) {
     // help function to clarify commands used
     case "help":
         console.log("Please type one of these commands\n"+
-                    "'concert-this': to search your favorite artist concerts\n"+
-                    "'spotify-this-song': to search your favorite song\n"+
-                    "'movie-this': to search your favorite movie \n"+
-                    "'do-what-it-says': using command from random.txt \n"
+                    " *concert-this': to search your favorite artist concerts\n"+
+                    " *spotify-this-song': to search your favorite song\n"+
+                    " *movie-this': to search your favorite movie \n"+
+                    " *do-what-it-says': using command from random.txt \n"
                     );
         break;
     case "concert-this":
@@ -44,34 +46,36 @@ switch (program) {
         break;
     //if anything else written
     default:
-        console.log("LIRI doesn't understand that - Please type 'node liri.js help' for more information"
-                    );
+        console.log("LIRI doesn't understand that - Please type 'node liri.js help' for more information");
 };
 
 //Concert function
 function myConcert(nodeArgs) {
     var artist = nodeArgs;
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id="+ keys.concert.id;
-    // This line is just to help us debug against the actual URL.
-    //console.log(queryUrl);
-    axios.get(queryUrl).then(
-        function (response) {
-            // console.log(response.data[0])
-            // console.log("Venue Time: "+moment(response.data[0].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
-            for (var i = 0; i < 10; i++) {
+    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id="+ keys.concert.id;
+    // console.log(queryUrl);
+    axios.get(queryUrl).then(function (response) {
+        // console.log(response.data[0])
+        // console.log("Venue Time: "+moment(response.data[0].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
+        if (response.data == 0) {
+            var retreivingOriginalData = artist.split('+').join(' ')
+            console.log("We couldnt find concerts for " + retreivingOriginalData + "; Please change the Band name and search again")
+        } else {
+            for (var i = 0; i < 5; i++) {
                 console.log("Venue Name: " + response.data[i].venue.name);
                 console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
                 console.log("Venue Time: " + moment(response.data[0].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
                 console.log('--------------------------------------------------')
+                //adds output to log.txt
                 fs.appendFileSync('log.txt', "\r\n" + "Concert Search Log-------------------------------------------------" + "\r\n");
+                fs.appendFileSync('log.txt', "\r\n" + "Command used: node liri.js " + command + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "Venue Name: " + response.data[i].venue.name + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "Venue Time: " + moment(response.data[0].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A') + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "----------------------------------------------------------------" + "\r\n");
             }
         }
-    );
-
+    });
 };
 
 
@@ -79,42 +83,46 @@ function myConcert(nodeArgs) {
 function mySpotify(nodeArgs){
     //if there are no arguments transfer the user to a default search
     if (!nodeArgs){
-        	nodeArgs = 'The Sign';
+        	nodeArgs = 'the+sign';
     	}
-		spotify.search({ type: 'track', query: nodeArgs }, function(err, data) {
+        // var song = process.argv.slice(3).join("+");
+		spotify.search({ type: 'track', query: nodeArgs, limit: 4 }, function(err, data) {
 			if (err){
 	            console.log('Error occurred: ' + err);
 	            return;
 	        }
-	        var songData = data.tracks.items;
+	        let songData = data.tracks.items;
 	        console.log("Artist(s): " + songData[0].artists[0].name);
 	        console.log("Song Name: " + songData[0].name);
 	        console.log("Preview Link: " + songData[0].preview_url);
 	        console.log("Album: " + songData[0].album.name);
-            //adds text to log.txt
+            //adds output to log.txt
             fs.appendFileSync('log.txt', "\r\n" + "Song Search Log-------------------------------------------------"+ "\r\n", 'utf8');
+            fs.appendFileSync('log.txt', "\r\n" + "Command used: node liri.js " + command + "\r\n");
             fs.appendFileSync('log.txt', "\r\n" + "Artist(s): " + songData[0].artists[0].name + "\r\n", 'utf8');
             fs.appendFileSync('log.txt', "\r\n" + "Song Name: " + songData[0].name + "\r\n", 'utf8' );
             fs.appendFileSync('log.txt', "\r\n" + "Preview Link: " + songData[0].preview_url+ "\r\n", 'utf8' );
             fs.appendFileSync('log.txt', "\r\n" + "Album: " + songData[0].album.name+ "\r\n", 'utf8');
             fs.appendFileSync('log.txt', "\r\n" + "----------------------------------------------------------------"+ "\r\n", 'utf8');
-	});
+    });
 }
 
 
 //search movies function
-function myMovies(nodeArgs) {
+function myMovies(nodeArgs, command) {
+    let movieName = nodeArgs;
     //if there are no arguments transfer the user to a default search
     if (!nodeArgs){
-        	nodeArgs = 'Mr Nobody';
+        	movieName = 'Mr Nobody';
     	}
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey="+ keys.omdb.id;
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + keys.omdb.id;
+    
     // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
+    // console.log(queryUrl);
     axios.get(queryUrl).then(
         function (response) {
-            if (!error && response.statusCode === 200) {
-                var jsonData = JSON.parse(body);
+            // if (response.statusCode === 200) {
+                var jsonData = response.data;
                 console.log("Title: " + jsonData.Title);
                 console.log("Release Year: " + jsonData.Year);
                 console.log("IMDB Rating: " + jsonData.imdbRating);
@@ -123,8 +131,10 @@ function myMovies(nodeArgs) {
                 console.log("Language: " + jsonData.Language);
                 console.log("Plot: " + jsonData.Plot);
                 console.log("Actors: " + jsonData.Actors);
+                // console.log(""+command)
                 //adds text to log.txt
                 fs.appendFileSync('log.txt', "\r\n" + "Movie Search Log-----------------------" + "\r\n");
+                fs.appendFileSync('log.txt', "\r\n" + "Command used: node liri.js " + command + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "Title: " + jsonData.Title + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "Release Year: " + jsonData.Year + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "IMDB Rating: " + jsonData.imdbRating + "\r\n");
@@ -135,7 +145,7 @@ function myMovies(nodeArgs) {
                 fs.appendFileSync('log.txt', "\r\n" + "Actors: " + jsonData.Actors + "\r\n");
                 fs.appendFileSync('log.txt', "\r\n" + "---------------------------------------" + "\r\n");
 
-            }
+            // }
         }
     );
 
@@ -151,19 +161,19 @@ function doWhatItSays() {
   		}
 
 		// Then split it by commas (to make it more readable)
-		var dataArr = data.split(",");
+		let dataArr = data.split(",");
 
 		// Each command is represented. Because of the format in the txt file, remove the quotes to run these commands. 
 		if (dataArr[0] === "spotify-this-song") {
-			var songcheck = dataArr[1].slice(1, -1);
+			let songcheck = dataArr[1].slice(1, -1);
             console.log("songCheck: "+songcheck)
 			mySpotify(songcheck);
 		} else if (dataArr[0] === "concert-this") {
-			var venueName = dataArr[1].slice(1, -1);
+			let venueName = dataArr[1].slice(1, -1);
             console.log("venueName: "+venueName)
 			myConcert(venueName);
 		} else if(dataArr[0] === "movie-this") {
-			var movieName = dataArr[1].slice(1, -1);
+			let movieName = dataArr[1].slice(1, -1);
             console.log("movieName: "+movieName)
 			myMovies(movieName);
 		} 
